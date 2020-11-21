67e509eebb96dabc7c52ae9f77e6b0aa3e3e2dc6
lucas.hornung <lucashornung@hotmail.com>
2020-11-21 18:35:48 +0100

    implemented design changes

---
+++ tf-code/S3.tf
@@ -0,0 +1,68 @@
+resource "aws_s3_bucket" "dataBucket" {
+  bucket = "dataBucket58366"
+  lifecycle {
+    prevent_destroy = true
+  }
+  versioning {
+    enabled = true
+  }
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+
+
+}
+
+resource "aws_s3_bucket" "reverseDataBucket" {
+  bucket = "reverseBucket58366"
+  lifecycle {
+    prevent_destroy = true
+  }
+  versioning {
+    enabled = true
+  }
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+
+}
+
+resource "aws_s3_bucket" "lambdaBucket" {
+  bucket = "lambdaBucket58366"
+  lifecycle {
+    prevent_destroy = true
+  }
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+
+}
+
+resource "aws_s3_bucket" "terraform_state" {
+  bucket = "terraformstatebucket58366"
+  lifecycle {
+    prevent_destroy = true
+  }
+  versioning {
+    enabled = true
+  }
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+}
---
+++ tf-code/lambda.tf
@@ -0,0 +1,29 @@
+/*
+data "aws_s3_bucket_object" "reverse_data_lambda_function_hash" {
+  bucket = "bucket-for-lambda-zips"
+  key    = "path/reverse_data_lambda_function_payload.zip.base64sha256"
+}
+
+resource "aws_lambda_function" "s3_copy_reversed_data_function" {
+  filename = "lambda.zip"
+  source_code_hash = data.archive_file.my_lambda_function.output_base64sha256
+  function_name = "${var.env_name}_s3_copy_reversed_data_lambda"
+  role = aws_iam_role.s3_copy_function.arn
+  handler = "index.handler"
+  runtime = "python3.7"
+
+  environment {
+    variables = {
+      DST_BUCKET = "${var.env_name}-dst-bucket",
+      REGION = var.region
+    }
+  }
+}
+
+data "archive_file" "my_lambda_function" {
+  source_dir  = "${path.module}/lambda/"
+  output_path = "${path.module}/lambda.zip"
+  type        = "zip"
+}
+
+*/
