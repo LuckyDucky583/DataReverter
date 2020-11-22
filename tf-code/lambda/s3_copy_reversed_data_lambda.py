c4367643cd0f03dc8afbeca8c5729e23e6560b7d
lucas.hornung <lucashornung@hotmail.com>
2020-11-22 01:48:57 +0100

    added reverse data element

--- tf-code/lambda/s3_copy_reversed_data_lambda.py
+++ tf-code/lambda/s3_copy_reversed_data_lambda.py
@@ -3,11 +3,11 @@
 import os
 import logging
 import boto3
+import botocore
 
 LOGGER = logging.getLogger()
 LOGGER.setLevel(logging.INFO)
 
-DST_BUCKET = boto3.resource('reversebucket58366')
 REGION = os.environ.get('REGION')
 
 s3 = boto3.resource('s3', region_name=REGION)
@@ -15,21 +15,24 @@ s3 = boto3.resource('s3', region_name=REGION)
 
 def handler(event, context):
     LOGGER.info('Event structure: %s', event)
-    LOGGER.info('DST_BUCKET: %s', DST_BUCKET)
 
     for record in event['Records']:
         src_bucket = record['s3']['bucket']['name']
-        print(src_bucket)
         src_key = record['s3']['object']['key']
-        print(src_key)
 
-        copy_source = {
+        original = {
             'Bucket': src_bucket,
             'Key': src_key
         }
-        LOGGER.info('copy_source: %s', copy_source)
-        bucket = s3.Bucket('reversebucket58366')
-        bucket.copy(copy_source, src_key)
+
+        text = s3.Object(src_bucket, src_key)
+        data = text.get()['Body'].read().decode('utf-8')
+        data_1 = data[::-1]
+        LOGGER.info(data_1)
+
+        LOGGER.info('copy_source: %s', data_1)
+       # s3.upload_file(data_1, 'reversebucket58366', src_key + '-reversed')
+        s3.Object('reversebucket58366', src_key + '-reversed').put(Body=data_1)
 
     return {
         'status': 'ok'
